9524a25926faeee01af1e10924e493e8a191e910
adil <amedeuyev@gmail.com>
2024-09-13 20:03:13 +0500

    Initial Commit

---
+++ GR sync/GR_syncApp.swift
@@ -0,0 +1,17 @@
+//
+//  GR_syncApp.swift
+//  GR sync
+//
+//  Created by Adilkhan Medeuyev on 13.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GR_syncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
